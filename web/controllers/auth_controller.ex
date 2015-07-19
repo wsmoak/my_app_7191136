a0852a51df24fd37a88071ccfff2a489f1e8524c
Wendy Smoak <wsmoak@gmail.com>
2015-07-19 17:21:54 -0400

    Remove hard-coded url and ask the strategy for the authorize url

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,6 +2,6 @@ defmodule MyApp_7191136.AuthController do
   use MyApp_7191136.Web, :controller
 
   def index(conn, _params) do
-    redirect conn, external: "https://www.fitbit.com/oauth2/authorize"
+    redirect conn, external: Fitbit.authorize_url!(scope: "settings sleep")
   end
 end
